639b8900772ed14e03b81d7a8342c90dd9bf9b6b
Mael Drelon <mael.drelon1@gmail.com>
2019-12-09 17:21:14 +0100

    Update

--- Personnage.php
+++ Personnage.php
@@ -4,21 +4,30 @@ class Personnage
     private $_pseudo;
     private $_vie;
     private $_force;
-    private $_islife;
+    private $_alive;
 
     public function __construct($valeurvie,$laforce,$lepseudo)
     {
         $this->_pseudo = $lepseudo;
         $this->_vie = $valeurvie;
         $this->_force = $laforce;
-        $this->_islife = true;
+        $this->_alive = true;
     }
 
     public function AfficherPersonnage()
     {
         echo "<p>Je suis ".$this->_pseudo.', </p> ';
-        echo "<p>J'ai ".$this->_vie.' de santé et '.$this->_force,' point de dégat</p>';
+        echo "<p>J'ai ".$this->_vie.' de santé.';
+    }
+    
+    public function Attaquer($ObjetPersonnage)
+    {
+        echo "<p>".$this->_pseudo." attaque ".$ObjetPersonnage->getNom();
+        $this->_vie = $this->_vie-50;
     }
 
-
+    public function alive()
+    {
+       $ObjetPersonnage = $this->_alive;
+    }
 }
--- index.php
+++ index.php
@@ -6,11 +6,25 @@
 </head>
 <body>
 <?php echo "<h1>You encounter an foe!</h1>";
-$Personnage1 = new Personnage(100,25,"Mimos");
+$Personnage1 = new Personnage(250,25,"Mimos");
 $Personnage2 = new Personnage(200,10,"Banshee");
 
 $Personnage1->AfficherPersonnage();
 $Personnage2->AfficherPersonnage();
+
+echo "<p>Lancement du combat</p>";
+while( $Personnage1->alive() && $Personnage2->alive())
+{
+    if(rand(0, 1))
+    {
+        $Personnage1->Attaquer($Personnage2);
+    }    
+    else
+    {
+        $Personnage2->Attaquer($Personnage1);
+    }
+}
+
 
 ?>
 
