7806fd243cc5985a53904ed5a8dd7e00868969d2
Mael Drelon <mael.drelon1@gmail.com>
2020-01-23 11:12:02 +0100

    update

--- User.php
+++ User.php
@@ -1,1 +1,39 @@
 <?php
+class User    
+{
+    private$_id;
+    private$_Nom;
+    
+    public function _construct($id,$nom)
+    {
+        try 
+        {
+            $base = new PDO('mysql:host=localhost; dbname=BaseDeDonneePersonnage', 'root', 'root');
+            $DonneeBruteUser = $base->query("SELECT * from Personnages");
+            $TabUserIndex = 0;
+            while ($tab = $DonneeBruteUser->fetch())
+            {
+                $TabUser[$TabUserIndex++] = new User($tab['ID_User'],$tab['Nom']);        
+            }     
+
+        }
+    catch(exception$e) 
+        {
+            $e->getMessage();
+        }
+
+    $this->_id = $id;
+    $this->_Nom = $nom;  
+    }
+
+    public function getId()
+    {
+        return $this->_id;        
+    }
+
+    public function getNom()
+    {
+        return $this->_Nom;        
+    }    
+}
+?>
--- indexSTANDBY.php
+++ indexSTANDBY.php
@@ -5,7 +5,9 @@
 <head>
 </head>
 <body>
-<?php echo "<h1>You encounter a foe!</h1>";
+
+<?php 
+echo "<h1>You encounter a foe!</h1>";
 $Personnage1 = new Personnage(250,25,"Mimos");
 $Personnage2 = new Personnage(200,10,"Banshee");
 
@@ -14,18 +16,42 @@ $Personnage2->AfficherPersonnage();
 
 echo "<p>Lancement du combat</p>";
 
-    if(rand(0, 1))
-    {
-        $Personnage1->Attaquer($Personnage2);
-    }    
-    else
-    {
-        $Personnage2->Attaquer($Personnage1);
-    }
-
-
-
+if(rand(0, 1))
+{
+    $Personnage1->Attaquer($Personnage2);
+}    
+else
+{
+    $Personnage2->Attaquer($Personnage1);
+}
 ?>
+
+<FORM action="" methode="POST">
+<select name="Personnage"id="Perso-select">
+<?php 
+foreach ($TabUser as $objetUser) 
+{
+    echo'<option value="'.$objetUser->getId().'">'.$objetUser->getNom().'</option>';                
+}
+if (isset($_POST["user"]))
+{
+    foreach ($TabUser as $objetUser) 
+    {
+        if ($objetUser->getId()==$_POST["user"])
+        {
+            $objetUser->afficherUser();            
+        }       
+    }      
+}
+else
+{
+    echo "Aucun user selectionné";
+}
+?>
+</select>
+<input type="submit">
+</input>
+</FORM>
 
 
 
